654e5f2727426d9d9427459ec199308b88980aac
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-06-13 09:33:16 -0700

    portfolio tier 1 done

---
+++ assets/js/navbar.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("burger").style.display="none";
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    
+  }
+  
+  function closeNav() {
+    document.getElementById("burger").style.display="block";
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
+
+
+ 
